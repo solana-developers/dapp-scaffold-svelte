413da0a208d8243bcfb1c0985913c476d704b85b
silvestrevivo <silvestrevivomillan@gmail.com>
2022-08-25 12:01:45 +0200

    refact(adapter): torus support added, ui improved and sveltekit updated

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import inject from '@rollup/plugin-inject';
+import nodePolyfills from "rollup-plugin-node-polyfills";
 import path from 'path';
 
 /** @type {import('vite').UserConfig} */
@@ -11,17 +13,26 @@ const config = {
 		'process.env.VERSION': JSON.stringify(process.env.npm_package_version)
 	},
 	optimizeDeps: {
-		include: ['@solana/web3.js', 'buffer']
+		include: ['@solana/web3.js', 'buffer'],
+		esbuildOptions: {
+				target: 'esnext',
+				plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })],
+		},
 	},
 	resolve: {
 		alias: {
-			$stores: path.resolve('./src/stores')
+			$stores: path.resolve('./src/stores'),
+			stream: 'rollup-plugin-node-polyfills/polyfills/stream',
 		}
 	},
 	build: {
+		target: 'esnext',
+		commonjsOptions: {
+			transformMixedEsModules: true
+		},
 		rollupOptions: {
-			plugins: [inject({ Buffer: ['buffer', 'Buffer'] })]
-		}
+			plugins: [inject({ Buffer: ['buffer', 'Buffer'] }), nodePolyfills({ crypto: true })],
+		},
 	},
 	server: {
 		host: true
